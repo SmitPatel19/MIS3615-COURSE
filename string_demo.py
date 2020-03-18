c3b84096269c59138cf4ceb42abdf16a687ca62e
Spatel4 <57647348+SmitPatel19@users.noreply.github.com>
2020-03-18 13:30:00 -0400

    Finish 2nd part, intoduced to F (formating outputs) and string manipulation

---
+++ string_demo.py
@@ -0,0 +1,22 @@
+print(2-1)
+
+#print('2'-'1') INCORRECT
+
+#print('EU'-'Great Britain') INCORRECT
+
+first_name = 'Smit'
+last_name = 'Patel'
+name = first_name + ' ' + last_name
+print(name)
+
+print('Naah, na na nananan naah, naahaha, hey Jude.\n' * 10)\
+
+print(f'Hello, {name}!')
+
+actor = 'Joaquin Phoenix'
+year = 2020
+movie = 'Joker'
+print(f'{actor} wins Best Actor for {movie} at Golden Globes {year}')
+
+pi = 3.14159265358
+print(f'Pi equals {pi:.3}')
