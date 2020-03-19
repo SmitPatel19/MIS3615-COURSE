15da8b124088f617f22b694f0f1e05f40505c9a7
Spatel4 <57647348+SmitPatel19@users.noreply.github.com>
2020-03-18 23:12:23 -0400

    Create Chapter 3 - Data Types.py

---
+++ Chapter 3 - Data Types.py
@@ -0,0 +1,22 @@
+import math
+print(math.pi)
+print(math.sqrt(85))
+
+import random
+print(random.random())
+print(random.choice([1,2,3,4]))
+
+
+print('I\'m learning\nPython.')
+
+3>2
+3>5
+
+age = input('Please enter your age: ')
+if age >= 21:
+    print('Yes, you can.')
+else:
+    print('Sorry, nice try bud haha!')
+
+    
+
