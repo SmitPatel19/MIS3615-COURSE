7fcd34d217e1d9edc016ecd203f227ac70f15b49
Spatel4 <57647348+SmitPatel19@users.noreply.github.com>
2020-03-30 16:50:44 -0400

    Create lists_demo.py

---
+++ lists_demo.py
@@ -0,0 +1,20 @@
+a = []
+print(type(a))
+
+print(len(a))
+
+a = [10,20,30,40]
+
+print(len(a))
+
+b = ['New England Patriots', 'Buffalo Bills', 'Miami Dolphins', 'New York Jets']
+print(len(b))
+print(type(b))
+print(type(b[0]))
+print(len(b[0]))
+
+c = ['spam', 2.0, 5, [10, 20]]
+print(type(c))
+print(type(c[0]))
+print(type(c[-1]))
+
