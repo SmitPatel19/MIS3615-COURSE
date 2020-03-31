5f064b8fcdccfd10498a1ce1ee6f66438a793874
Spatel4 <57647348+SmitPatel19@users.noreply.github.com>
2020-03-31 00:13:03 -0400

    Create json_excercise.py

---
+++ json_excercise.py
@@ -0,0 +1,25 @@
+import urllib.request
+import json
+import pprint
+
+APIKEY = '51e01fc87a6f69d3db031cf74510b937'
+city = 'Wellesley'
+country_code = 'us'
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&APPID={APIKEY}'
+
+# print(url)
+f = urllib.request.urlopen(url)
+response_text = f.read().decode('utf-8')
+response_data = json.loads(response_text)
+pprint.pprint(response_data)
+
+# Can you get the current temperature in Wellesley?
+
+
+#print(response_data['main'])
+
+#for temp in response_data['main']:
+    #print(temp[0])
+
+#print(response_data['main'[temp[0]]])
+
